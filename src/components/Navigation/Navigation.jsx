1ae09cead4cf63765733ce83154005b2aa266dc0
n1k1x86 <i@n1k1x86.ru>
2023-01-14 19:14:04 +0300

    Включено использование <NavLink /> для избежание перезагрузки страницы при переходе между компонентами Profile и Dialogs

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import style from './Navigation.module.css';
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
     return (
         <nav className={style.nav}>
             <div className={style.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile" className={navData => navData.isActive ? style.active : style.item}>Profile</NavLink>
             </div>
             <div className={style.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" className={navData => navData.isActive ? style.active : style.item}>Messages</NavLink>
             </div>
             <div className={style.item}>
                 <a>News</a>
